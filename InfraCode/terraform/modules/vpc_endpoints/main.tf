69d33fcbf28ed03be3b5a9cadd297d9e3d090448
Kiran Gonela <kirangonela@Kirans-MacBook-Pro.local>
2023-09-28 15:56:20 +0100

    first commit-- updated

--- InfraCode/terraform/modules/vpc_endpoints/main.tf
+++ InfraCode/terraform/modules/vpc_endpoints/main.tf
@@ -22,6 +22,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = var.vpc_id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
+  route_table_ids   = var.private_route_table_ids
   #subnet_ids        = var.private_subnet_ids
 }
 
